1ae2ea80afa2e3b594c08c7fd34d6973cd4b1fd0
maxim <uzumaxy@gmail.com>
2015-03-11 14:13:37 +0200

    Use graph builder.

--- app/client/views/core_visualization/core_visualization.js
+++ app/client/views/core_visualization/core_visualization.js
@@ -26,52 +26,12 @@ Template.CoreVisualization.created = function () {
 };
 
 Template.CoreVisualization.rendered = function () {
-  var allPackages = [];
-  _.each(App.corePackages, function(dependencies, packageName) {
-    if (allPackages.indexOf(packageName) === -1) {
-      allPackages.push(packageName);
-    }
-    _.each(dependencies, function(depName) {
-      if (allPackages.indexOf(depName) === -1) {
-        allPackages.push(depName);
-      }
-    });
-  });
-
-  var nodes = _.map(allPackages, function(packageName) {
-    var size = 20;
-    if (!!App.corePackages[packageName]) {
-      size += 10 + App.corePackages[packageName].length;
-    }
-    return {
-      id: 'pkg-' + packageName,
-      label: packageName,
-      x: Math.random(),
-      y: Math.random(),
-      size: size,
-      color: '#777'
-    };
-  });
-
-  var edges = [];
-  _.each(App.corePackages, function(dependencies, packageName) {
-    _.each(dependencies, function(depName) {
-      edges.push({
-        id: packageName + '-dep-' + depName,
-        source: 'pkg-' + packageName,
-        target: 'pkg-' + depName,
-        size: Math.random(),
-        color: '#ccc',
-        type: 'arrow'
-      });
-    });
-  });
-
+  var graph = new GraphBuilder().build(App.corePackages);
   // Instantiate sigma:
   new sigma({
     graph: {
-      nodes: nodes,
-      edges: edges
+      nodes: graph.nodes,
+      edges: graph.edges
     },
     renderer: {
       container: document.getElementById('graph-container'),
